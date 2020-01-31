b13120dcadba1b3ec39cb9273879cc8aa082504e
rprass <rprass@gmail.com>
2020-01-31 12:22:33 -0200

    Alterção na descrição do projeto

--- Rockets/src/Teste.java
+++ Rockets/src/Teste.java
@@ -4,7 +4,7 @@ public class Teste {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Teste Github");
+		System.out.println("Teste Github versão 1.0");
 		
 
 	}
